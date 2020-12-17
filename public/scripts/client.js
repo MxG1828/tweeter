4855d1feba174d78bbae6f6a7c574523b6501e95
Michael Xiang <yorkmike4@gmail.com>
2020-12-17 11:45:54 +0000

    submit and fetch tweets with ajax, added escape function, error display for tweet form, toggle button for tweet form

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,55 +3,73 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+
 $(document).ready(function () {
-  let tweetData = [
-    {
-      user: {
-        name: "Newton",
-        avatars: "https://i.imgur.com/73hZDYK.png",
-        handle: "@SirIsaac",
-      },
-      content: {
-        text: "If I have seen further it is by standing on the shoulders of giants",
-      },
-      created_at: 1607928810558,
-    },
-    {
-      user: {
-        name: "Descartes",
-        avatars: "https://i.imgur.com/nlhLi3I.png",
-        handle: "@rd",
-      },
-      content: {
-        text: "Je pense , donc je suis",
-      },
-      created_at: 1608015210558,
-    },
-  ];
+  $("form").on("submit", (event) => {
+    event.preventDefault();
+    $(".error").slideUp();
+    //check if input is empty or over 140
+    if ($("form").children("textarea").val().length === 0) {
+      return $("#emptyErr").slideDown();
+    }
+    if ($("form").children("textarea").val().length > 140) {
+      return $("#exceedErr").slideDown();
+    }
+    //ajax post input to server .then get the input back and put it on top.
+    $.ajax({
+      url: "/tweets/",
+      data: $("form").serialize(),
+      method: "POST",
+    }).then(() => {
+      $.ajax({ url: "/tweets/", method: "GET" }).then((tweets) => {
+        $("#tweetBlocks").prepend(createTweetElement(tweets[tweets.length - 1]));
+      });
+      $("form").children("textarea").val("");
+      $(".counter").val(140);
+    });
+  });
+
+  const loadTweets = function () {
+    $.ajax({ url: "/tweets/", method: "GET" }).then((tweets) => {
+      renderTweets(tweets);
+    });
+  };
+  loadTweets();
+
   const renderTweets = (tweets) => {
-    for (let tweet of tweets) {
-      const $tweet = createTweetElement(tweet);
+    for (let i = tweets.length - 1; i >= 0; i--) {
+      const $tweet = createTweetElement(tweets[i]);
       $("#tweetBlocks").append($tweet);
     }
   };
-
+  const escape = function (str) {
+    let div = document.createElement("div");
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  };
   const createTweetElement = (tweet) => {
+    const userName = escape(tweet.user.name);
+    const content = escape(tweet.content.text);
+    const userHandle = escape(tweet.user.handle);
     const $tweet = $(`
-      <article class="tweet">
-        <header>
-          <p><img src=${tweet.user.avatars} alt="user_icon" style="height:50px; width:50px">${tweet.user.name}</p>
-          <p class="handle">${tweet.user.handle}</p>
-        </header>
-        <div>
-          <p>${tweet.content.text}</p>
-        </div>
-        <footer>
-          <p>${timeago.format(tweet.created_at)}</p>
-        </footer>
-      </article>
-      `);
-
+    <article class="tweet">
+      <header>
+        <p><img src=${tweet.user.avatars} alt="user_icon" style="height:50px; width:50px">${userName}</p>
+        <p class="handle">${userHandle}</p>
+     </header>
+      <div>
+        <p>${content}</p>
+      </div>
+      <footer>
+        <p>${timeago.format(tweet.created_at)}</p>
+      </footer>
+    </article>
+    `);
     return $tweet;
   };
-  renderTweets(tweetData);
+  $("#tweetForm").on("click",(evt)=>{
+    $(".new-tweet").slideToggle({complete: ()=>{
+      $("#tweet-text").focus();
+    }})
+  });
 });
